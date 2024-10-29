04a50ca50d1b6fd5ab1e9ebb6b02c47eb5b535f6
Sofia Vaca Angulo <sofia.vacaa@gmail.com>
2024-10-29 21:15:42 +0100

    Update R script test.R
    Sofia trying hihi

--- src/R script test.R
+++ src/R script test.R
@@ -1,7 +1,9 @@
 ### test by Julian and Maela for nice Github Project ###
 
-ok ok
+##ok ok
 
-Here writing random stuff to see if we can see soemthing happening
+##Here writing random stuff to see if we can see soemthing happening
 #i'm modifying
 #
+
+##hi im sofia im trying to modify too
