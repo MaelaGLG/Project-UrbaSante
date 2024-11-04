c1b19c56c0ff7b5dd586a53cf736834e18c63947
MaelaGLG <maela.guillaumelegall@sciencespo.fr>
2024-11-04 13:01:41 +0100

    adding my script

--- src/R script test.R
+++
@@ -1,9 +0,0 @@
-### test by Julian and Maela for nice Github Project ###
-
-##ok ok
-
-##Here writing random stuff to see if we can see soemthing happening
-#i'm modifying
-#
-
-##hi im sofia im trying to modify too
